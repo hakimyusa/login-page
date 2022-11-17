a41f0cc430a40cac0750bdb1138209395676d35b
Muhammad Salman Al-Hakim <113969088+hakimyusa@users.noreply.github.com>
2022-11-17 22:54:54 +0700

    logout redirect
    logout redirect

---
+++ logout.php-redirect.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Redirect...</title>
+	<meta http-equiv="refresh" content="3;url=index.php">
+</head>
+<body>
+<p>Telah berhasil Logout, anda akan dialihkan ke halaman login.</p>
+</body>
+</html>
+
